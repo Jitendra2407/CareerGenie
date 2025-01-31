d18dacc4a480e6ae23bc7a0ce0c793f87a09b787
Jitendra2407 <jitendrakumarthakur552@gmail.com>
2025-01-31 16:48:08 +0530

    authentication and header

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <div>Subscribe to RoadsideCoder</div>
+      <br/>
+      <Button>Hello</Button>
+    </div>
+  );
+}
