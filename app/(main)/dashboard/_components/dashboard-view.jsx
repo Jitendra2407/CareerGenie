264156fec147e766010dcf81b13ef43ff45571d3
Jitendra2407 <jitendrakumarthakur552@gmail.com>
2025-02-08 23:26:10 +0530

    resolving error

--- app/(main)/dashboard/_components/dashboard-view.jsx
+++ app/(main)/dashboard/_components/dashboard-view.jsx
@@ -4,7 +4,25 @@ import React from 'react'
 
 const DashboardView = ({insights}) => {
 
-  const salaryData = insights.salaryData
+  const salaryData = insights.salaryRanges.map((range) => ({
+    name: range.role,
+    min: range.min / 1000,
+    max: range.max / 1000,
+    median: range.median / 1000
+  }));
+
+  const getDemandLevelColor = (level) => {
+    switch (level.toLowerCase()){
+      case "high":
+        return "bg-green-500";
+      case "medium":
+        return "bg-yellow-500";
+      case "low":
+        return "bg-red-500";    
+    }
+  };
+
+  
 
   return <div>DashboardView</div>;
 }
